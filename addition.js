a76263b04d9d30d52fb5a019061e2d242b8ce9ee
veroniquex1 <vdrossouw@gmail.com>
2024-02-07 10:05:17 +0200

    addition function

--- addition.js
+++ addition.js
@@ -1,1 +1,15 @@
 console.log("Addition");
+
+// function addition (a, b){
+//     return a + b
+// }
+
+// to export multiple functions at the same time
+// module.exports = {
+//     addition
+// }
+
+// short cut
+exports.addition = (numb1, numb2) =>{
+    return numb1 + numb2
+}
